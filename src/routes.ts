559c8710d4a505d4473d0249bd648fc17948e4f7
LC_ <lclaudio.tl@gmail.com>
2024-05-03 21:45:25 -0300

    Iniciando projeto backend e listando materias

---
+++ src/routes.ts
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const routes = Router()
+const prisma = new PrismaClient()
+
+routes.get('/materias', async (req, res) => {
+
+  const materias = await prisma.materia.findMany()
+
+  return res.json(materias)
+})
+
+routes.post('/materia', async (req, res) => {
+  const data = req.body
+  console.log(data)
+//  await prisma.materia.create({
+//     data: {
+//       codigo: 'adsf',
+//       nome: 'Elsa Prisma',
+//     },
+//   })
+
+  return res.json({message: "Cadastro realizado"})
+})
+
+export default routes;
