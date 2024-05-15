4c32897bc5c73fa155f257c99bdff616200eef1b
Guibs969 <guilhermeferreirabrandao969@gmail.com>
2024-05-14 21:22:56 -0300

    Coloquei model de prof

--- src/server.ts
+++ src/server.ts
@@ -6,4 +6,4 @@ const app = express();
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333, () => console.log('Server is running on port 3333 🚀'));
+app.listen(3333, () => console.log('Tá funfando  🚀'));
